d4a36e32580e2d94f9b773162b978b320a350e2b
Vancouver <serebrennikov1988@gmail.com>
2024-02-25 12:28:54 +0300

    add lesson1:task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a = 35;
+int b = 91;
+int c = 46;
+int d = 56;
+int e = 6;
+
+int max = a;
+
+if (b > max)
+{
+	max = b;
+}
+if (c > max)
+{
+	max = c;
+}
+if (d > max)
+{
+	max = d;
+}
+if (e > max)
+{
+	max = e;
+}
+
+Console.WriteLine(max);
