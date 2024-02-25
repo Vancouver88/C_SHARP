5d5117e5f71aabdb62df497eb0e21b5c22df2fb6
Vancouver <serebrennikov1988@gmail.com>
2024-02-25 18:13:00 +0300

    add lesson2:hw4

---
+++ Lesson2/HW4/Program.cs
@@ -0,0 +1,16 @@
+﻿int num = int.Parse(Console.ReadLine()!);
+
+//string digits = String.Join(", ", num.ToString().ToCharArray());
+
+//Console.WriteLine(digits);
+
+string digits = "";
+
+while (num > 0)
+{
+	int digit = num % 10;
+	digits = digit + ", " + digits;
+	num = num / 10;
+}
+
+Console.WriteLine(digits.TrimEnd(',', ' '));
