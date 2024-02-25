207e37080e4025b1efda8018b6e7c713dc16e0a3
Vancouver <serebrennikov1988@gmail.com>
2024-02-25 17:01:57 +0300

    add lesson2:hw3

---
+++ Lesson2/HW3/Program.cs
@@ -0,0 +1,16 @@
+﻿int num = int.Parse(Console.ReadLine()!);
+int first_num = num / 10;
+int second_num = num % 10;
+
+if (num < 10 || num > 99)
+{
+	Console.WriteLine("Некорректное число");
+}
+else if (first_num > second_num)
+{
+	Console.WriteLine(first_num);
+}
+else
+{
+	Console.WriteLine(second_num);
+}
