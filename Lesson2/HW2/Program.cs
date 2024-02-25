0b28cfe1739e7a63bf36f3594e47673932db3612
Vancouver <serebrennikov1988@gmail.com>
2024-02-25 16:47:39 +0300

    add lesson2:hw2

---
+++ Lesson2/HW2/Program.cs
@@ -0,0 +1,23 @@
+﻿int X = int.Parse(Console.ReadLine()!);
+int Y = int.Parse(Console.ReadLine()!);
+
+if (X == 0 || Y == 0)
+{
+	Console.WriteLine("Вы находитесь на оси координат");
+}
+else if (X > 0 && Y > 0)
+{
+	Console.WriteLine("Вы находитесь в 1 четверти");
+}
+else if (X < 0 && Y > 0)
+{
+	Console.WriteLine("Вы находитесь во 2 четверти");
+}
+else if (X < 0 && Y < 0)
+{
+	Console.WriteLine("Вы находитесь в 3 четверти");
+}
+else if (X > 0 && Y < 0)
+{
+	Console.WriteLine("Вы находитесь в 4 четверти");
+}
