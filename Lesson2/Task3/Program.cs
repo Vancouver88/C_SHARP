907f146ab532e99a0bd9dc88f9477eb5e3067f79
Vancouver <serebrennikov1988@gmail.com>
2024-02-25 16:16:22 +0300

    add lesson2:task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 % num2 == 0)
+{
+	Console.WriteLine("да");
+}
+else
+{
+	Console.WriteLine("нет, остаток " + num1 % num2);
+}
