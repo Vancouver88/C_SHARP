6587b03e09b85da4ac41757c6bb3728eae227586
Vancouver <serebrennikov1988@gmail.com>
2024-02-25 13:58:12 +0300

    add lesson1:task6

---
+++ Lesson1/Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("num1: ");
+string var1 = Console.ReadLine();
+int var_num1 = int.Parse(var1);
+Console.Write("num2: ");
+string var2 = Console.ReadLine();
+int var_num2 = int.Parse(var2);
+
+if (var_num1 == var_num2 * var_num2)
+	Console.Write("Да");
+else
+	Console.Write("Нет");
