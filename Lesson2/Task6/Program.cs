bbdc150be4d9f13154f285a187d0867527850073
Vancouver <serebrennikov1988@gmail.com>
2024-03-09 21:17:37 +0300

    lesson2:task6

---
+++ Lesson2/Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.Length)
+{
+	arr[i] = i + 1;
+	//Console.Write(arr[i]);
+	//Console.Write(' ');
+	Console.Write($"{arr[i]} ");
+	i = i + 1;
+}
+
+//Solution 3
+//Try to check if the Omnisharp extension for C# has the correct settings, once you have downloaded this extension.
+
+//Click 'Ctrl + Shift + P'.
+//Write "OmniSharp: Select Project" and press Enter.
+//Choose the solution workspace entry.
+//Then enable the C# extension for "OmniSharp: Project".
