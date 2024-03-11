e1daf72e515dedd1b95ada76907520741bdcb158
Vancouver <serebrennikov1988@gmail.com>
2024-03-11 17:42:59 +0300

    lesson3s:task1

---
+++ Lesson3 semi/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] arr = { 2, 6, 9, 12, 16 };
+int num = int.Parse(Console.ReadLine()!);
+string answ = "False";
+for (int i = 0; i < arr.Length; i++)
+{
+	if (arr[i] == num)
+		answ = "True";
+}
+Console.Write(answ);
