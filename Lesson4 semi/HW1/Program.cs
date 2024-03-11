52fdff33b07e0ec9a2d03ade8636ff6615f68ead
Vancouver <serebrennikov1988@gmail.com>
2024-03-11 20:06:31 +0300

    lesson4s:HW

---
+++ Lesson4 semi/HW1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		while (true)
+		{
+			Console.WriteLine("Введите целое число или 'q' для выхода:");
+			string input = Console.ReadLine();
+
+			if (input.ToLower() == "q")
+			{
+				break;
+			}
+
+			int number;
+			if (int.TryParse(input, out number))
+			{
+				if (SumOfDigitsIsEven(number))
+				{
+					Console.WriteLine($"Сумма цифр числа {number} чётная. Программа завершена.");
+					break;
+				}
+			}
+			else
+			{
+				Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+			}
+		}
+	}
+
+	static bool SumOfDigitsIsEven(int number)
+	{
+		int sum = 0;
+
+		foreach (char digit in Math.Abs(number).ToString())
+		{
+			sum += int.Parse(digit.ToString());
+		}
+
+		return sum % 2 == 0;
+	}
+}
