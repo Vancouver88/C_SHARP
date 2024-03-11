40f62a8e288ea665aeb96573f9ee75a58ab51993
Vancouver <serebrennikov1988@gmail.com>
2024-03-11 20:46:46 +0300

    lesson3l:task1

---
+++ Lesson3 lec/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+	double numenator = a * b;
+	int denumenator = c * d;
+	double result = numenator / denumenator;
+	return result;
+}
+
+double result = CalculateFormula(5, 6, 8, 9);
+Console.Write(result);
