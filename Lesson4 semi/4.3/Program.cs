358ed7b3def2d3c69474ac4b43fee1fa276e8d8b
Vancouver <serebrennikov1988@gmail.com>
2024-03-18 22:40:13 +0300

    lesson4s:4.3

---
+++ Lesson4 semi/4.3/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] CreateArray(int size)
+{
+	Random rnd = new Random();
+	int[] arr = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rnd.Next(0, 9);
+	}
+	return arr;
+}
+
+int CreateNumber(int[] arr)
+{
+	string FinalNumber = "";
+	foreach (int item in arr)
+	{
+		FinalNumber = FinalNumber + item;
+	}
+	return int.Parse(FinalNumber);
+}
+
+Console.WriteLine("Введите размер блока:");
+int size = int.Parse(Console.ReadLine()!);
+
+if (size > 8)
+{
+	Console.Write("Не правильный размер массива!");
+}
+else
+{
+	int[] result = CreateArray(size);
+	Console.WriteLine(string.Join(", ", result));
+	Console.WriteLine(CreateNumber(result));
+}
