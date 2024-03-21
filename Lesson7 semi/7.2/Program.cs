44aa8cd7400bc4b787dab1e88c77d1aecf970d9e
Vancouver <serebrennikov1988@gmail.com>
2024-03-21 15:48:06 +0300

    lesson7s:7.2

---
+++ Lesson7 semi/7.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+void FindSumNum(int a)
+{
+	if (a == 0)
+	{
+		return;
+	}
+	FindSumNum(a - 1);
+	Console.Write(a + " ");
+}
+
+FindSumNum(a);
