9c486a83c36fce95095003c163c0c94c206069da
Vancouver <serebrennikov1988@gmail.com>
2024-03-21 15:40:26 +0300

    lesson7s:7.1

---
+++ Lesson7 semi/7.1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+int FindSumNum(int a)
+{
+	if (a == 0)
+	{
+		return 0;
+	}
+	return FindSumNum(a / 10) + a % 10;
+}
+
+Console.Write(FindSumNum(a));
