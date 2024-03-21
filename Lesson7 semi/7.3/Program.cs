ae7788b3d1ee5680098b7a2e8e79d45c1f61ad80
Vancouver <serebrennikov1988@gmail.com>
2024-03-21 16:02:11 +0300

    lesson7s:7.3

---
+++ Lesson7 semi/7.3/Program.cs
@@ -0,0 +1,15 @@
+﻿string vowle = "aeyuio";
+Console.WriteLine("Введите слово: ");
+string word = Console.ReadLine()!;
+
+void PrintChars(string firstWord, int i = 0)
+{
+	if (i == firstWord.Length) return;
+	if (vowle.Contains(firstWord[i]) == false)
+	{
+		Console.Write(firstWord[i] + " ");
+	}
+	PrintChars(firstWord, i + 1);
+}
+
+PrintChars(word);
