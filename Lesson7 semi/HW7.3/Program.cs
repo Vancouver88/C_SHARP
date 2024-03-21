c58f8ef03454145bfdae2d3b9c14c9b23e928140
Vancouver <serebrennikov1988@gmail.com>
2024-03-21 17:17:44 +0300

    lesson7s:hw7.3

---
+++ Lesson7 semi/HW7.3/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] arr = { 1, 2, 5, 0, 10, 34 };
+
+void PrintArrayRevers(int[] arr, int i)
+{
+	if (i < 0) return;
+	Console.Write(arr[i] + " ");
+	PrintArrayRevers(arr, i - 1);
+}
+
+PrintArrayRevers(arr, arr.Length - 1);
