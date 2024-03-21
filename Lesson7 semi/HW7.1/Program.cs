13c476e95613912ba5f65aa0b64bdfa5ccfa0adf
Vancouver <serebrennikov1988@gmail.com>
2024-03-21 16:20:18 +0300

    lesson7s:HW7.1

---
+++ Lesson7 semi/HW7.1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+void PrintChars(int M, int N)
+{
+	if (N == M - 1)
+	{
+		return;
+	}
+	PrintChars(M, N - 1);
+	Console.Write(N + " ");
+}
+
+PrintChars(M, N);
